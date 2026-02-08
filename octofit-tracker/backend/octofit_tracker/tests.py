dc8dd9578154a3e7ccb0b2cfd5b7c10d0c041c91
Sabyasachi Das <41511474+neWbie-saby@users.noreply.github.com>
2026-02-08 10:18:07 +0000

    2nd list of prompts successful ~Claude+GPT

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel', is_superhero=True)
+        self.assertEqual(user.email, 'test@hero.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Marvel')
+        self.assertEqual(team.name, 'Marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel', is_superhero=True)
+        activity = Activity.objects.create(user=user, type='Running', duration=30, date='2026-02-08')
+        self.assertEqual(activity.type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        leaderboard = Leaderboard.objects.create(team='Marvel', points=100)
+        self.assertEqual(leaderboard.team, 'Marvel')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for='Marvel')
+        self.assertEqual(workout.name, 'Pushups')
